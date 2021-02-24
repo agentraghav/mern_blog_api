2f26053a3bfa9294cf45d80f6019a896793a1920
agentraghav <raghvendrarathore2@gmail.com>
2021-02-24 12:58:45 +0530

    server updates

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
+const cors = require('cors');
 require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
